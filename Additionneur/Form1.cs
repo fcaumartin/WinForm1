2b4a3cb9090a817b9cd7660cb62635e3e71a84d6
frc <fcaumartin@gmail.com>
2016-08-22 10:42:51 +0200

    premier commit

---
+++ Additionneur/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Additionneur
+{
+    public partial class Form1 : Form
+    {
+        int somme = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "+1";
+            somme += 1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "+2";
+            somme += 2;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "+3";
+            somme += 3;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "=" + somme;
+
+        }
+    }
+}
